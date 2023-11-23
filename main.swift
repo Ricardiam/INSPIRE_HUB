476ca6093b99cf90422a6e68edb18298b105b1bb
DoogieTech <93832900+Ricardiam@users.noreply.github.com>
2023-11-22 22:58:10 -0300

    1

---
+++ main.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func uploadVideo() {
+    // Lógica de carga de video aquí
+}
+
+func playVideo(videoId: Int) {
+    // Lógica de reproducción de video aquí
+    print("Reproduciendo video con ID: \(videoId)")
+}
+
+uploadVideo()
+playVideo(videoId: 1234)
